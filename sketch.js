1dfec83bb0c7b5983cbc53b00c65dbfc94fd1dd6
krish406 <155498135+krish406@users.noreply.github.com>
2024-12-25 13:37:57 -0500

    added the ability to change the image resolution

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
-let margin = 3;
-let num_squares = 16;
 const canvas = document.querySelector(".canvas");
+
+//properties of each individual square
+let margin = 1;
+let num_squares = 16;
 
 function removeCanvas(){
     let canvasContainer = document.querySelector("body");
@@ -8,21 +10,29 @@ function removeCanvas(){
 }
 
 function darkenSquare(pixel, darkenIncrement){
+    //each pixel has an opacity property created when it was built through the setAttribute method
+    //this will just store a string, so its copy needs to be converted to a float
     let currentOpacity = parseFloat(pixel.getAttribute("opacity"));
-    console.log(currentOpacity);
+    
+    //code onle runs while it is possible to darken the square
     if((currentOpacity + darkenIncrement) <= 1){
+        //adds the darken to the opacity and applies operation to fix javascript addition error
         let newOpacity = Math.round((currentOpacity + darkenIncrement) * 10) / 10;
+        
         pixel.style.backgroundColor = "grey";
         pixel.style.opacity = newOpacity;
+
+        //change the property of the pixel to reflect its new opacity
         pixel.setAttribute("opacity", newOpacity);
-        console.log(pixel.getAttribute("opacity"));
     }
 }
 
 function buildCanvas(square_width){
+    //there is margin width created on either side of the canvas
     canvas_width = (square_width + margin * 2) * num_squares
     canvas.style.width = `${canvas_width}px`;
-    //console.log(canvas.style.width);
+
+    //row, column use of nested loops
     for(let i = 0; i < num_squares; i++){
         for(let j = 0; j < num_squares; j++){
             const pixel = document.createElement("div");
@@ -30,6 +40,7 @@ function buildCanvas(square_width){
             pixel.style.height = `${square_width}px`;
             pixel.style.margin = `${margin}px`;
             pixel.style.backgroundColor = "white";
+
             //sets custom html element property called opacity
             pixel.setAttribute("opacity", 0);
 
